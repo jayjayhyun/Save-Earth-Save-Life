cb0f39e885f71f71fa3d1f124632d0951af1fc33
chanden <92653791+Chanden220@users.noreply.github.com>
2024-07-25 12:31:09 +0700

    update templates

--- Save_Earth_Save_Life/homepage/urls.py
+++ Save_Earth_Save_Life/homepage/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    # path('products/', views.products),
-    # path('customer/', views.customer)
+    path('', views.home,name="home"),
+    path('blog',views.blog,name="blog"),
+    path('blog_detail',views.blog_details,name="blog_details"),
+    path('service_details',views.service_details,name="service_details"),
+    path('starter_page',views.starter_page,name="starter_page"),
 ]
--- Save_Earth_Save_Life/homepage/views.py
+++ Save_Earth_Save_Life/homepage/views.py
@@ -3,4 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request,'landingpage.html')
+    return render(request,'index.html')
+def blog(request):
+    return render(request,'blog.html')
+def blog_details(request):
+    return render(request,'blog-details.html')
+def service_details(request):
+    return render(request,'service-details.html')
+def starter_page(request):
+    return render(request,'starter-page.html')
