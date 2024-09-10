6d81a0976fbee53999ecfd92d9e8cbbc4e5805b2
jayjayhyun <138762092+jayjayhyun@users.noreply.github.com>
2024-09-11 05:25:43 +0700

    initial testing

--- Save_Earth_Save_Life/Save_Earth_Save_Life/settings.py
+++ Save_Earth_Save_Life/Save_Earth_Save_Life/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^f1n*(x*79@e6ih&t(dsa!q3!j&89oiv%fa_hlkvn41$*ynq#d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['saveearth-savelife.com', '127.0.0.1']
 
 
 # Application definition
